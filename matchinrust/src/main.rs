f369098110d91e4355aa39ec6dc302b80dc9350c
Kumar Mohit <mohit724196@gmail.com>
2024-01-02 14:35:51 +0530

    switch case in rust

---
+++ matchinrust/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // Example value to match against
+    let num = 8;
+
+    // Using match to handle different cases
+    match num {
+        7 => println!("Value is 7"),
+        8 => println!("Value is 8"),
+        9 => println!("Value is 9"),
+        _ => println!("Out of range"),
+    }
+}
