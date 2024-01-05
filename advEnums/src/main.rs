93e5fa0781539de92cac885167b9e1c07c8f331d
Kumar Mohit <mohit724196@gmail.com>
2024-01-05 15:45:36 +0530

    advenum

--- advEnums/src/main.rs
+++ advEnums/src/main.rs
@@ -1,3 +1,36 @@
+#[derive(Debug)]
+
+enum Gender{
+    Male,Female,Trans
+}
+#[derive(Debug)]
+struct Person {
+    name:String,
+    email:String,
+    age:i32,
+    gender:Gender
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Advance Enums!");
+
+    let person1=Person{
+        name:String::from("Mohit narayan"),
+        email:String::from("mojsjfsfhfushfiue.com"),
+        age:43,
+        gender:Gender::Male
+    };
+
+    println!("{:?}",person1);
+
+    let result = cal(3);
+    println!("{:?}", result);
+}
+
+fn cal(no:i32) -> Option<bool>{
+    if no % 2 == 0{
+        Some(true)
+    }else {
+        None
+    }
 }
