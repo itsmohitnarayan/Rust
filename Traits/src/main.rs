043f8cce36f947329b0c78ebf9945d12aef0f810
Kumar Mohit <mohit724196@gmail.com>
2024-01-06 14:13:56 +0530

    code

--- Traits/src/main.rs
+++ Traits/src/main.rs
@@ -1,3 +1,21 @@
+trait Printable {
+    fn print(&self);
+}
+
+struct Rectangle{
+    width: u32,
+    height: u32,
+}
+
+impl Printable for Rectangle {
+    fn print(&self) {
+        println!("Rectangle: {} x {}", self.width,self.height);
+
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Traits!");
+    let Rect1 = Rectangle{width: 19, height: 12};
+    Rect1.print();
 }
