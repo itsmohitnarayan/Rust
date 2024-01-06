6b53a27ab1f56ae12b67c3662b3c601903cd9161
Kumar Mohit <mohit724196@gmail.com>
2024-01-06 14:37:57 +0530

    code

--- Vector/src/main.rs
+++ Vector/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Vector!");
+
+    let mut abc = vec![1, 2, 3, 4, 5, 6];
+    println!("{:?}", abc);
+
+    abc.push(9);
+    println!("{:?}", abc);
+
+    // Insert the value 10 at index 2
+    abc.insert(2, 10);
+    println!("{:?}", abc);
 }
