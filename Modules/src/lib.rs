380737fbd00b465b312a1d8a67dc10f1feafd50c
Kumar Mohit <mohit724196@gmail.com>
2024-01-07 12:32:26 +0530

    code

--- Modules/src/lib.rs
+++ Modules/src/lib.rs
@@ -1,1 +1,9 @@
-pub fn
+pub fn print_my_message(){
+    println!("Hello here is my message");
+}
+pub fn print_my_message1(){
+    println!("Hello here is my second message");
+}
+pub fn print_my_message2(){
+    println!("Hello here is my Third message");
+}
--- Modules/src/main.rs
+++ Modules/src/main.rs
@@ -1,12 +1,40 @@
+mod lib;
+
 
 mod my_module {
     pub fn personal() {
         println!("Hello");
     }
+
 }
+
+mod my_mod2{
+    pub mod movie{
+        pub mod english{
+            pub fn play(name:String){
+                println!("Playing this movie {}", name);
+            }
+        }
+        pub mod hindi{
+            pub fn play1(name:String) {
+                println!("Playing this movie {}",name);
+            }
+        }
+    }
+}
+
+use my_mod2::movie::english::play;
+use my_mod2::movie::hindi::play1;
 
 fn main() {
     println!("Modules!");
 
     my_module::personal();
+
+    lib::print_my_message();
+    lib::print_my_message1();
+    lib::print_my_message2();
+
+    play("Iron man".to_string());
+    play1("Swadesh".to_string());
 }
