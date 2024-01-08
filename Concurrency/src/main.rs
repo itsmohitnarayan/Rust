ecc068827fa3fd59a4bec5bc1da2949831ddb46b
Kumar Mohit <mohit724196@gmail.com>
2024-01-08 14:56:12 +0530

    code

--- Concurrency/src/main.rs
+++ Concurrency/src/main.rs
@@ -1,3 +1,23 @@
+//importing some packages
+use std::thread;
+use std::time::Duration;
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Concurrency!");
+    // create a new thread to run parallel with main function
+    thread::spawn(||{
+        for i in 1..10 {
+            println!("The numbers are {} from the spawned thread! \n", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    //code run from main function
+    for i in 1..5 {
+        print!("The number are {} from the main thread \n",i);
+        thread::sleep(Duration::from_millis(1));
+    }
 }
