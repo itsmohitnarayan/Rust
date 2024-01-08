012a4d895bf0bb128d60e51209c427e4710276a8
Kumar Mohit <mohit724196@gmail.com>
2024-01-08 15:16:35 +0530

    code

--- WriteIntoFile/src/main.rs
+++ WriteIntoFile/src/main.rs
@@ -9,5 +9,7 @@ fn main() {
     //let's create a new file
     let mut my_file = File::create("Output.txt").expect("could not able to create file");
     //write into file
-    my_file.write_all(b"Welcome to the channel Mohit Narayan").expect("Not able to wrute into file")
+    my_file.write_all(b"Welcome to the channel Mohit Narayan").expect("Not able to wrute into file");
+    //append the data 
+    my_file.write_all(b"Hey, I am Mohit").expect("Not able to write");
 }
