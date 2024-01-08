a06add5a88992f2871e9a54196ce174b74f0b23b
Kumar Mohit <mohit724196@gmail.com>
2024-01-08 12:52:15 +0530

    Code

--- Hashmap/src/main.rs
+++ Hashmap/src/main.rs
@@ -1,3 +1,20 @@
+
+
+
+use std::{collections::HashMap, hash::Hash}; 
 fn main() {
-    println!("Hello, world!");
+    println!("Hashmap!");
+    //declaring new hashmap as a marks variable
+    let mut marks: HashMap<&str, i32> = HashMap::new();
+
+    //adding values to the hashmap
+    marks.insert("Rust", 80);
+    marks.insert("Java", 50);
+    marks.insert("Python", 90);
+    marks.insert("Javascript", 70);
+    marks.insert("C++", 90);
+
+
+    println!("{:?}", marks);
+
 }
