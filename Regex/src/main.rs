895f1483a65ef3c97c19ba02b3e4dec4ea911374
Kumar Mohit <mohit724196@gmail.com>
2024-01-09 15:39:30 +0530

    code

--- Regex/src/main.rs
+++ Regex/src/main.rs
@@ -1,3 +1,18 @@
+extern crate regex;
+
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Regex Example!");
+
+    //ex1
+    let Regex = Regex::new(r"w{6}").unwrap();
+
+    let myname = "Mohit";
+
+    println!("Found the correct: {}", re.is_match(myname));
+
+
+
+
 }
