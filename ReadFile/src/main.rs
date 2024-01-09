085f06df490fd5f33224a99bf5d7864d79a9e70e
Kumar Mohit <mohit724196@gmail.com>
2024-01-09 15:28:08 +0530

    Code

--- ReadFile/src/main.rs
+++ ReadFile/src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("ReadFile!");
+
+    //Open the file
+    let mut file = File::open(r"C:\Users\Mohit\Rust\ReadFile\src\intro.txt").expect("Not able to find the file");
+
+    // Read the File
+    let mut content = String::new();
+    //Process the File 
+    file.read_to_string(&mut content).expect("Not able to read the file");
+
+    println!("File contains this data {} \n", content);
 }
