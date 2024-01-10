bcdcd77551226218dd8891459272e67f8927991d
Kumar Mohit <mohit724196@gmail.com>
2024-01-10 20:16:00 +0530

    code

--- UnsafeRust/src/main.rs
+++ UnsafeRust/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut num = 42;
+
+    // Using unsafe block to dereference a raw pointer
+    unsafe {
+        let raw_ptr = &mut num as *mut i32;
+        *raw_ptr += 10;
+    }
+
+    println!("Updated value: {}", num);
 }
